b2390c72758df209f0daec662ccc96200bbe8e93
ge49jof <burak.bakkal@tum.de>
2021-11-15 09:21:43 +0100

    Logging added to KVStoreImpl and CacheManager.

--- src/main/java/de/tum/i13/server/kv/KVStoreImpl.java
+++ src/main/java/de/tum/i13/server/kv/KVStoreImpl.java
@@ -6,33 +6,32 @@ import de.tum.i13.server.kv.KVMessage.StatusType;
 import de.tum.i13.server.storageManagment.CacheManager;
 
 public class KVStoreImpl implements KVStore {
-	
-	 private static final Logger LOGGER = Logger.getLogger(KVStoreImpl.class.getName());
+
+	private static final Logger LOGGER = Logger.getLogger(KVStoreImpl.class.getName());
 
 	@Override
 	public KVMessage put(String key, String value) throws Exception {
-		if (value != null) {
-			try {
+		try {
+			if (value != null) {
 				if (CacheManager.getInstance().put(key, value)) {
+					LOGGER.fine("Put (insert) operation successfully executed on tuple: (" + key + ", " + value + ")");
 					return new KVMessageImpl(key, value, StatusType.PUT_SUCCESS);
 				} else {
+					LOGGER.fine("Put (update) operation successfully executed on tuple: (" + key + ", " + value + ")");
 					return new KVMessageImpl(key, value, StatusType.PUT_UPDATE);
 				}
-			} catch (Exception e) {
-				LOGGER.throwing("KVStoreImpl", "put", e); // FIXME think about it again
-				return new KVMessageImpl(key, value, StatusType.PUT_ERROR);
-			}
-		} else {
-			try {
+			} else {
 				if (CacheManager.getInstance().delete(key)) {
+					LOGGER.fine("Delete operation successfully executed on key: " + key);
 					return new KVMessageImpl(key, value, StatusType.DELETE_SUCCESS);
 				} else {
+					LOGGER.fine("Delete operation not successfully executed on key: " + key + " becauseof key could not found");
 					return new KVMessageImpl(key, value, StatusType.DELETE_ERROR);
 				}
-			} catch (Exception e) {
-				LOGGER.throwing("KVStoreImpl", "put", e); // FIXME think about it again
-				return new KVMessageImpl(key, value, StatusType.DELETE_ERROR);
 			}
+		} catch (Exception e) {
+			LOGGER.throwing("KVStoreImpl", "put on tuple: (" + key + ", "  + value + ")", e);
+			return new KVMessageImpl(key, value, StatusType.PUT_ERROR);
 		}
 	}
 
@@ -41,12 +40,14 @@ public class KVStoreImpl implements KVStore {
 		try {
 			String value = CacheManager.getInstance().get(key);
 			if (value != null) {
+				LOGGER.fine("Get operation found value :" + value + " for key: " + key );
 				return new KVMessageImpl(key, value, StatusType.GET_SUCCESS);
 			} else {
+				LOGGER.fine("Get operation could not found value for key: " + key );
 				return new KVMessageImpl(key, null, StatusType.GET_ERROR);
 			}
 		} catch (Exception e) {
-			LOGGER.throwing("KVStoreImpl", "get", e); // FIXME think about it again
+			LOGGER.throwing("KVStoreImpl", "get for key: " + key, e); // FIXME think about it again
 			return new KVMessageImpl(key, null, StatusType.GET_ERROR);
 		}
 	}
--- src/main/java/de/tum/i13/server/storageManagment/CacheManager.java
+++ src/main/java/de/tum/i13/server/storageManagment/CacheManager.java
@@ -3,18 +3,21 @@ package de.tum.i13.server.storageManagment;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class CacheManager implements DataManager {
+
+	private static final Logger LOGGER = Logger.getLogger(CacheManager.class.getName());
 
 	private static CacheManager instance;
 
 	private int cacheSize;
-	
+
 	private CacheDisplacementStrategy cds;
-	
+
 	private HashMap<String, Value> map;
-	
-	// it is for implementation FIFO
+
+	// it is for implementation of FIFO
 	private LinkedList<String> keyList;
 
 	private CacheManager() {
@@ -25,10 +28,13 @@ public class CacheManager implements DataManager {
 
 	synchronized public static void init(int cacheSize, CacheDisplacementStrategy cds) {
 		if (instance == null) {
+			LOGGER.fine("CacheManager is initialized with cache size:" + cacheSize
+					+ " and cache displacement strategy: " + cds);
+
 			instance = new CacheManager();
-			// TODO check path and create file if necessary
 			instance.cacheSize = cacheSize;
 			instance.cds = cds;
+
 		}
 	}
 
@@ -47,74 +53,106 @@ public class CacheManager implements DataManager {
 	 * @return value for the given key, null if key not found
 	 */
 	synchronized public String get(String key) {
-		// FIXME implement cache functionality
+		LOGGER.fine("Get operation executiong for key: " + key);
+
 		Value v = map.get(key);
 		if (v != null) {
+			LOGGER.fine("Get operation found value :" + v + " for key: " + key + "in cache.");
+
 			updateCache(key);
 			return v.data;
 		}
-		
-		String value =  DiskManager.getInstance().get(key);
+
+		String value = DiskManager.getInstance().get(key);
 		if (value != null) {
+			LOGGER.fine(
+					"Get operation found value :" + value + " for key: " + key + "in disk and inserted into cache.");
+
 			insertToCache(key, value);
 		}
-		
+
 		return value;
 	}
 
 	private void updateCache(String key) {
 		Value value = map.get(key);
+
+		LOGGER.fine(
+				"The value :" + value + " for key: " + key + " is updating according to cache displacement strategy.");
+
 		switch (cds) {
-			case LRU: value.compareValue = System.currentTimeMillis(); break;
-			case LFU: value.compareValue++; break;
+		case LRU:
+			value.compareValue = System.currentTimeMillis();
+			break;
+		case LFU:
+			value.compareValue++;
+			break;
+		case FIFO:
+			// Do nothing
+			break;
 		}
 	}
 
 	private void insertToCache(String key, String value) {
+		LOGGER.fine("The value :" + value + " for key: " + key
+				+ " is inserting to the cache, according to cache displacement strategy.");
+
 		if (map.size() >= cacheSize) {
 			removeFromCache();
 		}
-		
+
 		Value v = null;
-		
+
 		switch (cds) {
-			case FIFO: v = new Value(value, 0); keyList.add(key); break;
-			case LRU: v = new Value(value, System.currentTimeMillis()); break;
-			case LFU: v = new Value(value, 1); break;
+		case FIFO:
+			v = new Value(value, 0);
+			keyList.add(key);
+			break;
+		case LRU:
+			v = new Value(value, System.currentTimeMillis());
+			break;
+		case LFU:
+			v = new Value(value, 1);
+			break;
 		}
-		
+
 		map.put(key, v);
-		
+
 	}
 
 	private void removeFromCache() {
+		LOGGER.fine("Removing an item from the cache, according to cache displacement strategy.");
+
 		switch (cds) {
-			case FIFO: removeForFIFO(); break;
-			case LRU:
-			case LFU: removeForLRUOrLFU(); break;
+		case FIFO:
+			removeForFIFO();
+			break;
+		case LRU:
+		case LFU:
+			removeForLRUOrLFU();
+			break;
 		}
 	}
 
 	private void removeForFIFO() {
-		String key = keyList.removeFirst(); 
+		String key = keyList.removeFirst();
 		map.remove(key);
 	}
-	
+
 	private void removeForLRUOrLFU() {
 		String minKey = "";
 		long min = Long.MAX_VALUE;
-		
+
 		for (Map.Entry<String, Value> entry : map.entrySet()) {
 			if (entry.getValue().compareValue < min) {
 				minKey = entry.getValue().data;
 				min = entry.getValue().compareValue;
 			}
 		}
-		
-		map.remove(minKey);
-		
-	}
 
+		map.remove(minKey);
+
+	}
 
 	/**
 	 * Put to the file
@@ -125,18 +163,26 @@ public class CacheManager implements DataManager {
 	 *         return false
 	 */
 	synchronized public boolean put(String key, String value) {
+		LOGGER.fine("Put operation executing on value :" + value + " for key: " + key);
+
 		boolean isInsert = DiskManager.getInstance().put(key, value);
+
 		Value v = map.get(key);
 		if (v != null) {
+			LOGGER.fine("Put operation found value :" + value + " for key: " + key + " in cache and updating it.");
+
 			updateCache(key);
 			v.data = value;
 			map.put(key, v);
+
 		} else {
+			LOGGER.fine("Put operation could not found value :" + value + " for key: " + key
+					+ " in cache and inserting it into cache.");
+
 			insertToCache(key, value);
 		}
-		
+
 		return isInsert;
-		
 	}
 
 	/**
@@ -146,26 +192,30 @@ public class CacheManager implements DataManager {
 	 * @return
 	 */
 	synchronized public boolean delete(String key) {
+		LOGGER.fine("Delete operation executing on key: " + key);
+
 		boolean isRemoved = DiskManager.getInstance().delete(key);
-		
+
 		if (isRemoved) {
 			Value v = map.get(key);
 			if (v != null) {
+				LOGGER.fine("Delete operation found value: " + v + " for key: " + key + " and removing it.");
+
 				map.remove(key);
 				if (cds == CacheDisplacementStrategy.FIFO) {
-					keyList.remove(key);
+					keyList.remove(key); // O(n)
 				}
-			
+
 			}
 		}
-		
+
 		return isRemoved;
 	}
 
 	private static class Value {
 		String data;
 		long compareValue;
-		
+
 		public Value(String data, long compareValue) {
 			this.data = data;
 			this.compareValue = compareValue;
