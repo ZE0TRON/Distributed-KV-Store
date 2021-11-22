a01e37004b14eb81748dbe87daa0b5801e83899e
ge49jof <burak.bakkal@tum.de>
2021-11-22 09:28:07 +0100

    Tests changed according to the  new logic in disk management.

--- src/test/java/de/tum/i13/server/storageManagment/CacheManagerFIFOTest.java
+++ src/test/java/de/tum/i13/server/storageManagment/CacheManagerFIFOTest.java
@@ -1,256 +1,185 @@
-//package de.tum.i13.server.storageManagment;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.Mockito.lenient;
-//
-//import java.lang.reflect.Field;
-//
-//import de.tum.i13.server.kv.KVItem;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class CacheManagerFIFOTest implements CMTest {
-//
-//	@Mock
-//	private DiskManager diskManager;
-//
-//	@BeforeEach
-//	/**
-//	 *
-//	 * citation from:
-//	 * https://stackoverflow.com/questions/8256989/singleton-and-unit-testing
-//	 *
-//	 */
-//	public void resetSingleton() throws NoSuchFieldException, IllegalAccessException {
-//		Field instance = CacheManager.class.getDeclaredField("instance");
-//		instance.setAccessible(true);
-//		instance.set(null, null);
-//
-//		DiskManager.instance = diskManager;
-//	}
-//
-//	@Test
-//	public void testGetInstanceBeforeInitializeIt() {
-//		RuntimeException thrown = Assertions.assertThrows(RuntimeException.class, () -> {
-//			CacheManager.getInstance();
-//		});
-//
-//		assertEquals("Initialize CacheManager first!", thrown.getMessage());
-//	}
-//
-//	private CacheManagerFIFO create(int cahceSize) {
-//		CacheManagerFactory.create(cahceSize, CacheDisplacementStrategy.FIFO);
-//
-//		return (CacheManagerFIFO) CacheManager.getInstance();
-//	}
-//
-//	@Test
-//	public void testInitCacheSize100Strategy() {
-//		CacheManagerFIFO instance = create(100);
-//
-//		assertEquals(100, instance.cacheSize);
-//	}
-//
-//	@Test
-//	public void testGetKeyNotInCacheAndNotInDisk() {
-//		CacheManagerFIFO instance = create(100);
-//
-//		String key = "key";
-//		lenient().when(diskManager.get(key)).thenReturn(null);
-//
-//		KVItem item = instance.get(key);
-//
-//		assertEquals(null, item.value);
-//		assertEquals(0, instance.map.size());
-//		assertEquals(0, instance.keyList.size());
-//
-//	}
-//
-//	@Test
-//	public void testGetKeyNotInCacheAndInDisk() {
-//		CacheManagerFIFO instance = create(100);
-//
-//		String key = "k";
-//		String value = "v";
-//		lenient().when(diskManager.get(key)).thenReturn(value);
-//
-//		assertEquals(0, instance.map.size());
-//		assertEquals(0, instance.keyList.size());
-//
-//		String valueReturnFromCache = instance.get(key);
-//
-//		assertEquals(value, valueReturnFromCache);
-//
-//		assertEquals(1, instance.map.size());
-//		assertEquals(1, instance.keyList.size());
-//	}
-//
-//	// get in cache and in disk FIFO
-//	@Test
-//	public void testGetKeyInCacheAndInDisk() {
-//		CacheManagerFIFO instance = create(100);
-//
-//		String key = "k";
-//		String value = "v";
-//		lenient().when(diskManager.get(key)).thenReturn(value);
-//
-//		instance.put(key, value);
-//
-//		assertEquals(1, instance.map.size());
-//		assertEquals(1, instance.keyList.size());
-//
-//		String valueReturnFromCache = instance.get(key);
-//
-//		assertEquals(1, instance.map.size());
-//		assertEquals(1, instance.keyList.size());
-//
-//		assertEquals(value, valueReturnFromCache);
-//	}
-//
-//	@Test
-//	public void testPutKeyNotInCacheAndNotInDisk() {
-//		CacheManagerFIFO instance = create(100);
-//
-//		for (int i = 1; i <= 10; i++) {
-//			instance.put("k" + i, "v" + i);
-//		}
-//
-//		assertEquals(10, instance.map.size());
-//
-//		lenient().when(diskManager.put("k", "v")).thenReturn(true);
-//
-//		boolean isInserted = instance.put("k", "v");
-//
-//		assertEquals(11, instance.map.size());
-//		assertTrue(isInserted);
-//	}
-////
-//
-//	@Test
-//	public void testPutKeyInCacheAndNotInDisk() {
-//		CacheManagerFIFO instance = create(100);
-//
-//		for (int i = 1; i <= 10; i++) {
-//			instance.put("k" + i, "v" + i);
-//		}
-//
-//		assertEquals(10, instance.map.size());
-//
-//		lenient().when(diskManager.put("k1", "vNew")).thenReturn(true);
-//
-//		boolean isInserted = instance.put("k1", "vNew");
-//
-//		assertEquals(10, instance.map.size());
-//		assertTrue(isInserted);
-//		assertEquals("vNew", instance.get("k1"));
-//	}
-//
-//	// is it the same?
-//	@Test
-//	public void testPutKeyInCacheAndInDisk() {
-//		CacheManagerFIFO instance = create(100);
-//
-//		for (int i = 1; i <= 10; i++) {
-//			instance.put("k" + i, "v" + i);
-//		}
-//
-//		assertEquals(10, instance.map.size());
-//
-//		lenient().when(diskManager.put("k1", "vNew")).thenReturn(true);
-//
-//		boolean isInserted = instance.put("k1", "vNew");
-//
-//		assertEquals(10, instance.map.size());
-//		assertTrue(isInserted);
-//		assertEquals("vNew", instance.get("k1"));
-//
-//	}
-//
-//	// should delete method diskmanager be mocked?
-//	// should map size be 9?
-//	@Test
-//	@DisplayName("Delete key not in the cache and not in disk for FIFO")
-//	public void testDeleteKeyNotInCacheAndNotInDisk() {
-//		CacheManagerFIFO instance = create(100);
-//
-//		for (int i = 1; i <= 10; i++) {
-//			instance.put("k" + i, "v" + i);
-//		}
-//
-//		assertEquals(10, instance.map.size());
-//
-//		lenient().when(diskManager.delete("k1")).thenReturn(false);
-//
-//		boolean isDeleted = instance.delete("k1");
-//
-//		assertEquals(9, instance.map.size());
-//		assertFalse(isDeleted);
-//	}
-//
-//	// should map size be 9?
-//	@Test
-//	public void testDeleteKeyInCacheAndNotInDisk() {
-//		CacheManagerFIFO instance = create(100);
-//
-//		for (int i = 1; i <= 10; i++) {
-//			instance.put("k" + i, "v" + i);
-//		}
-//
-//		assertEquals(10, instance.map.size());
-//
-//		lenient().when(diskManager.get("k1")).thenReturn(null);
-//
-//		boolean isDeleted = instance.delete("k1");
-//
-//		assertEquals(10, instance.map.size());
-//		assertFalse(isDeleted);
-//	}
-//
-//	@Test
-//	public void testDeleteKeyInCacheAndInDisk() {
-//		CacheManagerFIFO instance = create(100);
-//
-//		for (int i = 1; i <= 10; i++) {
-//			instance.put("k" + i, "v" + i);
-//		}
-//
-//		assertEquals(10, instance.map.size());
-//
-//		lenient().when(diskManager.delete("k1")).thenReturn(true);
-//
-//		boolean isDeleted = instance.delete("k1");
-//
-//		assertEquals(9, instance.map.size());
-//		assertTrue(isDeleted);
-//	}
-//
-//	@Test
-//	public void testCheckExceedFor() {
-//
-//		CacheManagerFIFO instance = create(3);
-//
-//		for (int i = 1; i <= 3; i++) {
-//			instance.put("k" + i, "v" + i);
-//		}
-//
-//		assertEquals("k1", instance.keyList.get(0));
-//		assertEquals("k2", instance.keyList.get(1));
-//		assertEquals("k3", instance.keyList.get(2));
-//
-//		instance.put("k4", "v4");
-//
-//		assertEquals("k2", instance.keyList.get(0));
-//		assertEquals("k3", instance.keyList.get(1));
-//		assertEquals("k4", instance.keyList.get(2));
-//
-//		assertEquals(3, instance.map.size());
-//
-//	}
-//
-//}
+package de.tum.i13.server.storageManagment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.reflect.Field;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import de.tum.i13.server.kv.KVItem;
+
+public class CacheManagerFIFOTest implements CMTest {
+
+	@BeforeEach
+	/**
+	 *
+	 * citation from:
+	 * https://stackoverflow.com/questions/8256989/singleton-and-unit-testing
+	 *
+	 */
+	public void resetSingleton() throws NoSuchFieldException, IllegalAccessException {
+		Field instance = CacheManager.class.getDeclaredField("instance");
+		instance.setAccessible(true);
+		instance.set(null, null);
+	}
+
+	@Test
+	public void testGetInstanceBeforeInitializeIt() {
+		RuntimeException thrown = Assertions.assertThrows(RuntimeException.class, () -> {
+			CacheManager.getInstance();
+		});
+
+		assertEquals("Initialize CacheManager first!", thrown.getMessage());
+	}
+
+	private CacheManagerFIFO create(int cahceSize) {
+		CacheManagerFactory.create(cahceSize, CacheDisplacementStrategy.FIFO);
+
+		return (CacheManagerFIFO) CacheManager.getInstance();
+	}
+
+	@Test
+	public void testInitCacheSize100Strategy() {
+		CacheManagerFIFO instance = create(100);
+
+		assertEquals(100, instance.cacheSize);
+	}
+
+	@Test
+	public void testGetKeyNotInCache() {
+		CacheManagerFIFO instance = create(100);
+
+		String key = "k";
+
+		assertEquals(0, instance.map.size());
+		assertEquals(0, instance.keyList.size());
+
+		KVItem valueReturnFromCache = instance.get(key);
+
+		assertEquals(null, valueReturnFromCache);
+
+		assertEquals(0, instance.map.size());
+		assertEquals(0, instance.keyList.size());
+	}
+
+	// get in cache and in disk FIFO
+	@Test
+	public void testGetKeyInCache() throws Exception {
+		CacheManagerFIFO instance = create(100);
+
+		String key = "k";
+		String value = "v";
+
+		instance.put(new KVItem(key, value));
+
+		assertEquals(1, instance.map.size());
+		assertEquals(1, instance.keyList.size());
+
+		KVItem valueReturnFromCache = instance.get(key);
+
+		assertEquals(1, instance.map.size());
+		assertEquals(1, instance.keyList.size());
+
+		assertEquals(value, valueReturnFromCache.value);
+	}
+
+	@Test
+	public void testPutKeyNotInCache() throws Exception {
+		CacheManagerFIFO instance = create(100);
+
+		for (int i = 1; i <= 10; i++) {
+			instance.put(new KVItem("k" + i, "v" + i));
+		}
+
+		assertEquals(10, instance.map.size());
+
+		PersistType pt = instance.put(new KVItem("k", "v"));
+
+		assertEquals(11, instance.map.size());
+		assertEquals(PersistType.INSERT, pt);
+		assertEquals("v", instance.get("k").value);
+	}
+
+	// is it the same?
+	@Test
+	public void testPutKeyInCache() throws Exception {
+		CacheManagerFIFO instance = create(100);
+
+		for (int i = 1; i <= 10; i++) {
+			instance.put(new KVItem("k" + i, "v" + i));
+		}
+
+		assertEquals(10, instance.map.size());
+
+		PersistType pt = instance.put(new KVItem("k1", "vNew"));
+
+		assertEquals(10, instance.map.size());
+		assertEquals(PersistType.UPDATE, pt);
+		assertEquals("vNew", instance.get("k1").value);
+
+	}
+
+	// should delete method diskmanager be mocked?
+	// should map size be 9?
+	@Test
+	@DisplayName("Delete key not in the cache and not in disk for FIFO")
+	public void testDeleteKeyNotInCache() throws Exception {
+		CacheManagerFIFO instance = create(100);
+
+		for (int i = 1; i <= 10; i++) {
+			instance.put(new KVItem("k" + i, "v" + i));
+		}
+
+		assertEquals(10, instance.map.size());
+
+		PersistType pt = instance.delete("k");
+
+		assertEquals(10, instance.map.size());
+		assertEquals(PersistType.DELETE, pt);
+	}
+
+	// should map size be 9?
+	@Test
+	public void testDeleteKeyInCache() throws Exception {
+		CacheManagerFIFO instance = create(100);
+
+		for (int i = 1; i <= 10; i++) {
+			instance.put(new KVItem("k" + i, "v" + i));
+		}
+
+		assertEquals(10, instance.map.size());
+
+		PersistType pt = instance.delete("k1");
+
+		assertEquals(9, instance.map.size());
+		assertEquals(PersistType.DELETE, pt);
+	}
+
+	@Test
+	public void testCheckExceedFor() throws Exception {
+
+		CacheManagerFIFO instance = create(3);
+
+		for (int i = 1; i <= 3; i++) {
+			instance.put(new KVItem("k" + i, "v" + i));
+		}
+
+		assertEquals("k1", instance.keyList.get(0));
+		assertEquals("k2", instance.keyList.get(1));
+		assertEquals("k3", instance.keyList.get(2));
+
+		instance.put(new KVItem("k4", "v4"));
+
+		assertEquals("k2", instance.keyList.get(0));
+		assertEquals("k3", instance.keyList.get(1));
+		assertEquals("k4", instance.keyList.get(2));
+
+		assertEquals(3, instance.map.size());
+
+	}
+
+
+}
--- src/test/java/de/tum/i13/server/storageManagment/CacheManagerLRUTest.java
+++ src/test/java/de/tum/i13/server/storageManagment/CacheManagerLRUTest.java
@@ -1,265 +1,196 @@
-//package de.tum.i13.server.storageManagment;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static org.mockito.Mockito.lenient;
-//
-//import java.lang.reflect.Field;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class CacheManagerLRUTest implements CMTest {
-//
-//	@Mock
-//	private DiskManager diskManager;
-//
-//	@BeforeEach
-//	/**
-//	 *
-//	 * citation from:
-//	 * https://stackoverflow.com/questions/8256989/singleton-and-unit-testing
-//	 *
-//	 */
-//	public void resetSingleton() throws NoSuchFieldException, IllegalAccessException {
-//		Field instance = CacheManager.class.getDeclaredField("instance");
-//		instance.setAccessible(true);
-//		instance.set(null, null);
-//
-//		DiskManager.instance = diskManager;
-//
-//	}
-//
-//	@Test
-//	public void testGetInstanceBeforeInitializeIt() {
-//		RuntimeException thrown = Assertions.assertThrows(RuntimeException.class, () -> {
-//			CacheManager.getInstance();
-//		});
-//
-//		assertEquals("Initialize CacheManager first!", thrown.getMessage());
-//	}
-//
-//	private CacheManagerLRU create(int cahceSize) {
-//		CacheManagerFactory.create(cahceSize, CacheDisplacementStrategy.LRU);
-//
-//		return (CacheManagerLRU) CacheManager.getInstance();
-//	}
-//
-//	@Test
-//	public void testInitCacheSize100Strategy() {
-//		CacheManagerLRU instance = create(100);
-//		assertEquals(100, instance.cacheSize);
-//	}
-//
-//	// get not in cache and not in disk LRU
-//	@Test
-//	public void testGetKeyNotInCacheAndNotInDisk() {
-//		CacheManagerLRU instance = create(100);
-//
-//		String key = "key";
-//		lenient().when(diskManager.get(key)).thenReturn(null);
-//
-//		String value = instance.get(key);
-//
-//		assertEquals(null, value);
-//		assertEquals(0, instance.map.size());
-//	}
-//
-//	// get not in cache and in disk LRU
-//	@Test
-//	public void testGetKeyNotInCacheAndInDisk() {
-//		CacheManagerLRU instance = create(100);
-//
-//		String key = "k";
-//		String value = "v";
-//		lenient().when(diskManager.get(key)).thenReturn(value);
-//
-//		assertEquals(0, instance.map.size());
-//
-//		String valueReturnFromCache = instance.get(key);
-//
-//		assertEquals(value, valueReturnFromCache);
-//
-//		assertEquals(1, instance.map.size());
-//	}
-//
-//	// get in cache and in disk LRU
-//	@Test
-//	public void testGetKeyInCacheAndInDisk() {
-//		CacheManagerLRU instance = create(100);
-//
-//		String key = "k";
-//		String value = "v";
-//		lenient().when(diskManager.get(key)).thenReturn(value);
-//
-//		instance.put(key, value);
-//
-//		assertEquals(1, instance.map.size());
-//
-//		String valueReturnFromCache = instance.get(key);
-//
-//		assertEquals(1, instance.map.size());
-//
-//		assertEquals(value, valueReturnFromCache);
-//	}
-//
-//	@Test
-//	public void testPutKeyNotInCacheAndNotInDisk() {
-//		CacheManagerLRU instance = create(100);
-//
-//		for (int i = 1; i <= 10; i++) {
-//			instance.put("k" + i, "v" + i);
-//		}
-//
-//		assertEquals(10, instance.map.size());
-//
-//		lenient().when(diskManager.put("k", "v")).thenReturn(true);
-//
-//		boolean isInserted = instance.put("k", "v");
-//
-//		assertEquals(11, instance.map.size());
-//		assertTrue(isInserted);
-//	}
-//
-//	@Test
-//	public void testPutKeyInCacheAndNotInDisk() {
-//		CacheManagerLRU instance = create(100);
-//
-//		for (int i = 1; i <= 10; i++) {
-//			instance.put("k" + i, "v" + i);
-//		}
-//
-//		assertEquals(10, instance.map.size());
-//
-//		lenient().when(diskManager.put("k1", "vNew")).thenReturn(true);
-//
-//		boolean isInserted = instance.put("k1", "vNew");
-//
-//		assertEquals(10, instance.map.size());
-//		assertTrue(isInserted);
-//		assertEquals("vNew", instance.get("k1"));
-//	}
-//
-//	@Test
-//	public void testPutKeyInCacheAndInDisk() {
-//		CacheManagerLRU instance = create(100);
-//
-//		for (int i = 1; i <= 10; i++) {
-//			instance.put("k" + i, "v" + i);
-//		}
-//
-//		assertEquals(10, instance.map.size());
-//
-//		lenient().when(diskManager.put("k1", "vNew")).thenReturn(true);
-//
-//		boolean isInserted = instance.put("k1", "vNew");
-//
-//		assertEquals(10, instance.map.size());
-//		assertTrue(isInserted);
-//		assertEquals("vNew", instance.get("k1"));
-//	}
-//
-//	@Test
-//	public void testDeleteKeyNotInCacheAndNotInDisk() {
-//		CacheManagerLRU instance = create(100);
-//
-//		for (int i = 1; i <= 10; i++) {
-//			instance.put("k" + i, "v" + i);
-//		}
-//
-//		assertEquals(10, instance.map.size());
-//
-//		lenient().when(diskManager.delete("k1")).thenReturn(false);
-//
-//		boolean isDeleted = instance.delete("k1");
-//
-//		assertEquals(9, instance.map.size());
-//		assertFalse(isDeleted);
-//
-//	}
-//
-//	@Test
-//	public void testDeleteKeyInCacheAndNotInDisk() {
-//		CacheManagerLRU instance = create(100);
-//
-//		for (int i = 1; i <= 10; i++) {
-//			instance.put("k" + i, "v" + i);
-//		}
-//
-//		assertEquals(10, instance.map.size());
-//
-//		lenient().when(diskManager.get("k1")).thenReturn(null);
-//
-//		boolean isDeleted = instance.delete("k1");
-//
-//		assertEquals(10, instance.map.size());
-//		assertFalse(isDeleted);
-//	}
-//
-//	@Test
-//	public void testDeleteKeyInCacheAndInDisk() {
-//		CacheManagerLRU instance = create(100);
-//
-//		for (int i = 1; i <= 10; i++) {
-//			instance.put("k" + i, "v" + i);
-//		}
-//
-//		assertEquals(10, instance.map.size());
-//
-//		lenient().when(diskManager.delete("k1")).thenReturn(true);
-//
-//		boolean isDeleted = instance.delete("k1");
-//
-//		assertEquals(9, instance.map.size());
-//		assertTrue(isDeleted);
-//	}
-//
-//	@Test
-//	public void testCheckExceedFor() throws InterruptedException {
-//		CacheManagerLRU instance = create(3);
-//
-//		for (int i = 1; i <= 3; i++) {
-//			instance.put("k" + i, "v" + i);
-//			Thread.sleep(2);
-//		}
-//
-//		// Make k1 updated in the case, k2 is least recently used
-//		lenient().when(diskManager.get("k1")).thenReturn("v1");
-//		instance.get("k1");
-//
-//		instance.put("k4", "v4");
-//		assertFalse(instance.map.keySet().contains("k2"));
-//		assertEquals(3, instance.map.size());
-//
-//		Thread.sleep(2);
-//
-//		instance.put("k5", "v5");
-//		assertFalse(instance.map.keySet().contains("k3"));
-//		assertEquals(3, instance.map.size());
-//
-//		Thread.sleep(2);
-//
-//		lenient().when(diskManager.delete("k4")).thenReturn(true);
-//		instance.delete("k4");
-//		assertEquals(2, instance.map.size());
-//
-//		Thread.sleep(2);
-//
-//		instance.put("k6", "v6");
-//		assertTrue(instance.map.keySet().contains("k1"));
-//		assertEquals(3, instance.map.size());
-//
-//		Thread.sleep(2);
-//
-//		instance.put("k7", "v7");
-//		assertFalse(instance.map.keySet().contains("k1"));
-//		assertEquals(3, instance.map.size());
-//	}
-//
-//}
+package de.tum.i13.server.storageManagment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.lang.reflect.Field;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import de.tum.i13.server.kv.KVItem;
+
+public class CacheManagerLRUTest implements CMTest {
+
+	@BeforeEach
+	/**
+	 *
+	 * citation from:
+	 * https://stackoverflow.com/questions/8256989/singleton-and-unit-testing
+	 *
+	 */
+	public void resetSingleton() throws NoSuchFieldException, IllegalAccessException {
+		Field instance = CacheManager.class.getDeclaredField("instance");
+		instance.setAccessible(true);
+		instance.set(null, null);
+		
+	}
+
+	private CacheManagerLRU create(int cahceSize) {
+		CacheManagerFactory.create(cahceSize, CacheDisplacementStrategy.LRU);
+
+		return (CacheManagerLRU) CacheManager.getInstance();
+	}
+	
+
+
+	@Test
+	public void testGetInstanceBeforeInitializeIt() {
+		RuntimeException thrown = Assertions.assertThrows(RuntimeException.class, () -> {
+			CacheManager.getInstance();
+		});
+
+		assertEquals("Initialize CacheManager first!", thrown.getMessage());
+	}
+
+	@Test
+	public void testInitCacheSize100Strategy() {
+		CacheManagerLRU instance = create(100);
+		assertEquals(100, instance.cacheSize);
+	}
+
+
+	@Test
+	public void testGetKeyNotInCache() {
+		CacheManagerLRU instance = create(100);
+
+		String key = "k";
+
+		assertEquals(0, instance.map.size());
+
+		KVItem valueReturnFromCache = instance.get(key);
+
+		assertEquals(null, valueReturnFromCache);
+
+		assertEquals(0, instance.map.size());
+	}
+
+	@Test
+	public void testGetKeyInCache() throws Exception {
+		CacheManagerLRU instance = create(100);
+
+		String key = "k";
+		String value = "v";
+
+		instance.put(new KVItem(key, value));
+
+		assertEquals(1, instance.map.size());
+
+		KVItem valueReturnFromCache = instance.get(key);
+
+		assertEquals(1, instance.map.size());
+
+		assertEquals(value, valueReturnFromCache.value);
+	}
+
+	@Test
+	public void testPutKeyNotInCache() throws Exception {
+		CacheManagerLRU instance = create(100);
+
+		for (int i = 1; i <= 10; i++) {
+			instance.put(new KVItem("k" + i, "v" + i));
+		}
+
+		assertEquals(10, instance.map.size());
+
+		PersistType pt = instance.put(new KVItem("k", "v"));
+
+		assertEquals(11, instance.map.size());
+		assertEquals(PersistType.INSERT, pt);
+		assertEquals("v", instance.get("k").value);
+	}
+
+	@Test
+	public void testPutKeyInCache() throws Exception {
+		CacheManagerLRU instance = create(100);
+
+		for (int i = 1; i <= 10; i++) {
+			instance.put(new KVItem("k" + i, "v" + i));
+		}
+
+		assertEquals(10, instance.map.size());
+
+		PersistType pt = instance.put(new KVItem("k1", "vNew"));
+
+		assertEquals(10, instance.map.size());
+		assertEquals(PersistType.UPDATE, pt);
+		assertEquals("vNew", instance.get("k1").value);
+	}
+
+	@Test
+	public void testDeleteKeyNotInCache() throws Exception {
+		CacheManagerLRU instance = create(100);
+
+		for (int i = 1; i <= 10; i++) {
+			instance.put(new KVItem("k" + i, "v" + i));
+		}
+
+		assertEquals(10, instance.map.size());
+
+		PersistType pt = instance.delete("k");
+
+		assertEquals(10, instance.map.size());
+		assertEquals(PersistType.DELETE, pt);
+	}
+
+	@Test
+	public void testDeleteKeyInCache() throws Exception {
+		CacheManagerLRU instance = create(100);
+
+		for (int i = 1; i <= 10; i++) {
+			instance.put(new KVItem("k" + i, "v" + i));
+		}
+
+		assertEquals(10, instance.map.size());
+
+		PersistType pt = instance.delete("k1");
+
+		assertEquals(9, instance.map.size());
+		assertEquals(PersistType.DELETE, pt);
+
+	}
+
+	@Test
+	public void testCheckExceedFor() throws Exception {
+
+		CacheManagerLRU instance = create(3);
+
+		for (int i = 1; i <= 3; i++) {
+			instance.put(new KVItem("k" + i, "v" + i));
+			Thread.sleep(2);
+		}
+
+		instance.get("k1");
+
+		instance.put(new KVItem("k4", "v4"));
+		assertFalse(instance.map.keySet().contains("k2"));
+		assertEquals(3, instance.map.size());
+
+		Thread.sleep(2);
+
+		instance.put(new KVItem("k5", "v5"));
+		assertFalse(instance.map.keySet().contains("k3"));
+		assertEquals(3, instance.map.size());
+
+		Thread.sleep(2);
+
+		instance.delete("k4");
+		assertEquals(2, instance.map.size());
+
+		Thread.sleep(2);
+
+		instance.put(new KVItem("k6", "v6"));
+		assertTrue(instance.map.keySet().contains("k1"));
+		assertEquals(3, instance.map.size());
+
+		Thread.sleep(2);
+
+		instance.put(new KVItem("k7", "v7"));
+		assertFalse(instance.map.keySet().contains("k1"));
+		assertEquals(3, instance.map.size());
+		
+	}
+
+}
