67fb19f6c42129354bd7f97f40ae01ea886f1853
ge49jof <burak.bakkal@tum.de>
2022-01-28 18:21:38 +0300

    notification handling

--- src/main/java/de/tum/i13/shared/ConnectionManager/ClientNotificationThread.java
+++ src/main/java/de/tum/i13/shared/ConnectionManager/ClientNotificationThread.java
@@ -1,16 +1,17 @@
 package de.tum.i13.shared.ConnectionManager;
 
+import de.tum.i13.server.kv.KVClientMessage;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Locale;
 import java.util.logging.Logger;
 
 public class ClientNotificationThread extends Thread {
     private static final Logger LOGGER = Logger.getLogger(ClientNotificationThread.class.getName());
     private final ServerSocket CLIENT_NOTIFICATION_SOCKET;
-    private Socket serverNotificationSocket;
     private ConnectionManagerInterface connectionManager;
-
 
     public ClientNotificationThread(ServerSocket client_notification_socket) {
         CLIENT_NOTIFICATION_SOCKET = client_notification_socket;
@@ -20,12 +21,8 @@ public class ClientNotificationThread extends Thread {
     public void run(){
         LOGGER.info("ClientNotificationThread has been started. Waiting for ECS to connect.");
         try {
-            serverNotificationSocket = CLIENT_NOTIFICATION_SOCKET.accept();
+            Socket serverNotificationSocket = CLIENT_NOTIFICATION_SOCKET.accept();
             LOGGER.info("Server has connected to notification thread.");
-        } catch (IOException e) {
-            LOGGER.warning(e.getMessage());
-        }
-        try {
             this.connectionManager = new ConnectionManager(serverNotificationSocket);
         } catch (IOException e){
             LOGGER.warning(e.getMessage());
@@ -34,7 +31,17 @@ public class ClientNotificationThread extends Thread {
         try {
             String recv;
             while ( (recv = connectionManager.receive()) != null) {
-                System.out.println(recv);
+
+                String[] parts = recv.split(" ");
+                if (parts[0].equals(KVClientMessage.StatusType.NOTIFICATION_UPDATE.toString().toLowerCase(Locale.ROOT))) {
+                    // notification_update key value
+                    System.out.println("EchoClientNotification> UPDATE key: " + parts[1] + " new value: " + parts[2]);
+                } else if (parts[0].equals(KVClientMessage.StatusType.NOTIFICATION_DELETE.toString().toLowerCase(Locale.ROOT))) {
+                    // notification_delete key value
+                    System.out.println("EchoClientNotification> DELETE key: " + parts[1] + " value: " + parts[2]);
+                } else {
+                    throw new RuntimeException("Unknown notification type");
+                }
             }
         } catch (Exception e) {
             LOGGER.warning(e.getMessage());
