f5acb76bea1469ff28528e6f534705a7b69df08c
ZE0TRON <cmpbilge@gmail.com>
2022-01-28 20:14:23 +0100

    Docker compose in progress

--- docker/kvs/kvs.sh
+++ docker/kvs/kvs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "ecs address port is $ECS_PORT_55430_TCP_ADDR:$ECS_PORT_55430_TCP_PORT kv-server port is $1 and log level is $2"
+echo ecs
+echo "ecs address port is $ECS:$ecs kv-server port is $1 and log level is $2"
 #if [ -z ${+x} ];
 #  then echo "ECS ip:port should be set"; exit;
 #fi
@@ -7,6 +8,6 @@ if [ -z ${1+x} ];
   then echo "KV Server port should be set"; exit;
 fi
 if [ -z ${2+x} ];
-  then java -jar target/kv-server.jar -b "$ECS_PORT_55430_TCP_ADDR:$ECS_PORT_55430_TCP_PORT" -p $1;
+  then java -jar target/kv-server.jar -b ecs -p $1;
   else java -jar target/kv-server.jar -b "$ECS_PORT_55430_TCP_ADDR:$ECS_PORT_55430_TCP_PORT" -p $1 -ll $2;
 fi
